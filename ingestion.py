f317a454961e246301c40e2f75d3a0f1a6993ee2
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-05-21 23:53:38 +0530

    db  connection implementation

--- ingestion.py
+++ ingestion.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-
+from pymongo import MongoClient
 load_dotenv()
 
 
@@ -9,11 +9,15 @@ load_dotenv()
 
 class db_ingestion():
     def init(self):
-        db_credentials = os.getenv("DB_CREDENTIALS")        
+        db_credentials = os.getenv("DB_CREDENTIALS")      
+        if not db_credentials:
+            raise("DB credentials not found")
+        embedding_api_key = os.getenv("EMBEDDING_API_KEY")
+        if not embedding_api_key:
+            raise("embeddings api key not found")
+          
 
 
-
-print(db_credentials)
 
 
 
