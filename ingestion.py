a05b8512f5bd1b924b440e43303c59eb9cb4a61b
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-05-21 23:45:42 +0530

    first commit

---
+++ ingestion.py
@@ -0,0 +1,62 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+
+
+
+class db_ingestion():
+    def init(self):
+        db_credentials = os.getenv("DB_CREDENTIALS")        
+
+
+
+print(db_credentials)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
