8041194a83a8d58f8020a73294616e625efc5664
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-05-22 23:57:51 +0530

    db connection

--- ingestion.py
+++ ingestion.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 load_dotenv()
 
 
@@ -8,14 +9,34 @@ load_dotenv()
 
 
 class db_ingestion():
-    def init(self):
-        db_credentials = os.getenv("DB_CREDENTIALS")      
+    def __init__(self):
+        db_credentials = os.getenv("DB_CREDENTIALS")
+        print(db_credentials)      
         if not db_credentials:
-            raise("DB credentials not found")
+            raise Exception("DB credentials not found")
         embedding_api_key = os.getenv("EMBEDDING_API_KEY")
+        print(embedding_api_key)
         if not embedding_api_key:
-            raise("embeddings api key not found")
+            raise Exception("embeddings api key not found")
+        self.client = MongoClient(db_credentials, ServerSelectionTimeoutError=2000)
+        
+    def check_mongodb_connection(self):
+        try:
+            self.client.admin.command('ping')
+            print("✅ Success! Connected to MongoDB successfully.")
+        except ServerSelectionTimeoutError as e:
+            print("❌ Connection Failed! Could not connect to MongoDB server.")
+            print(f"Error details: {e}")   
+        except Exception as e:
+            print(f"an unexcepted error occured: {e}")
+        finally:
+            self.client.close()    
+        
+        
           
+if __name__=='__main__':
+    dbIngestion = db_ingestion()
+    dbIngestion.check_mongodb_connection()
 
 
 
